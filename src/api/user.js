39552ee25ef9eb3eaab5a0245b30cc8f9262221f
794774196 <794774196@qq.com>
2019-07-04 23:10:16 +0800

    first

---
+++ src/api/user.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  var res = request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+  console.log('user info:', res)
+  return res
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
