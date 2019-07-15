27a63b444cb1eafefd8c19f318afdd8d4e82c4ca
794774196 <794774196@qq.com>
2019-07-15 18:14:52 +0800

    small changes

--- src/views/components-demo/count-to.vue
+++ src/views/components-demo/count-to.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="components-container">
     <el-row :gutter="20" style="margin:30px;">
-      <el-col :span="6">
+      <el-col :span="7">
         <el-card class="box-card">
           <div slot="header" class="clearfix">
             <span>当前模型选择：
@@ -13,7 +13,7 @@
         </el-card>
       </el-col>
     </el-row>
-    <aside>
+    <!-- <aside>
       <a href="javascript:void(0)" target="_blank">队列模型</a>
     </aside>
     <div class="board">
@@ -24,7 +24,7 @@
     <aside>
       <a href="javascript:void(0)" target="_blank">最小费用最大流（MCMF）模型</a>
     </aside>
-    <canvas id="test-canvas" style="width:100%;height:300px"></canvas>
+    <canvas id="test-canvas" style="width:100%;height:300px"></canvas> -->
     <el-dialog v-el-drag-dialog :visible.sync="dialogTableVisible" title="调度模型" @dragDialog="handleDrag">
       <el-select ref="select" v-model="modelType" style="margin-top:0px;margin-bottom:20px;" placeholder="请选择调度模型">
         <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
@@ -39,7 +39,7 @@
 
 <script>
 import elDragDialog from '@/directive/el-drag-dialog' // base on element-ui
-import Kanban from '@/components/Kanban'
+// import Kanban from '@/components/Kanban'
 import JsonEditor from '@/components/JsonEditor'
 
 const jsonData = '[{"items":[{"market_type":"forexdata","symbol":"XAUUSD"},{"market_type":"forexdata","symbol":"UKOIL"},{"market_type":"forexdata","symbol":"CORN"}],"name":""}]'
@@ -48,20 +48,20 @@ export default {
   name: 'CountToDemo',
   directives: { elDragDialog },
   components: {
-    Kanban,
+    // Kanban,
     JsonEditor
   },
   data() {
     return {
-      schedulingType: '队列模型',
+      schedulingType: '未选择',
       group: 'mission',
       list1: [],
       list2: [],
       list3: [],
       dialogTableVisible: false,
       options: [
-        { value: '选项1', label: '队列模型' },
-        { value: '选项2', label: '最小费用最大流模型' }
+        { value: '队列模型', label: '队列模型' },
+        { value: '最小费用最大流模型', label: '最小费用最大流模型' }
       ],
       modelType: '',
       gridData: [{
@@ -85,21 +85,21 @@ export default {
     }
   },
   mounted() {
-    var can = document.getElementById('test-canvas')
-    var ctx = can.getContext('2d')
-    ctx.clearRect(0, 0, 200, 200)
-    ctx.fillStyle = '#dddddd'
-    ctx.fillRect(10, 10, 130, 130)
-    var path = new Path2D()
-    path.arc(75, 75, 50, 0, Math.PI * 2, true)
-    path.moveTo(110, 75)
-    path.arc(75, 75, 35, 0, Math.PI, false)
-    path.moveTo(65, 65)
-    path.arc(60, 65, 5, 0, Math.PI * 2, true)
-    path.moveTo(95, 65)
-    path.arc(90, 65, 5, 0, Math.PI * 2, true)
-    ctx.strokeStyle = '#0000ff'
-    ctx.stroke(path)
+    // var can = document.getElementById('test-canvas')
+    // var ctx = can.getContext('2d')
+    // ctx.clearRect(0, 0, 200, 200)
+    // ctx.fillStyle = '#dddddd'
+    // ctx.fillRect(10, 10, 130, 130)
+    // var path = new Path2D()
+    // path.arc(75, 75, 50, 0, Math.PI * 2, true)
+    // path.moveTo(110, 75)
+    // path.arc(75, 75, 35, 0, Math.PI, false)
+    // path.moveTo(65, 65)
+    // path.arc(60, 65, 5, 0, Math.PI * 2, true)
+    // path.moveTo(95, 65)
+    // path.arc(90, 65, 5, 0, Math.PI * 2, true)
+    // ctx.strokeStyle = '#0000ff'
+    // ctx.stroke(path)
 
     this.$store.dispatch('taskData/getAllTaskData').then((resolve, reject) => {
       console.log('resolve:', resolve)
@@ -110,8 +110,11 @@ export default {
   },
   methods: {
     clickA() {
-      this.schedulingType = '你选择过了'
       this.dialogTableVisible = true
+    },
+    clickB() {
+      this.dialogTableVisible = false
+      this.schedulingType = this.modelType
     },
     // v-el-drag-dialog onDrag callback function
     handleDrag() {
