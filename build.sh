c8cd8e6c555016586e482505586c5f9b9cff0dca
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2019-11-18 16:34:40 +0800

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker pull node:12.0.0
 docker build -t kubeext-dashboard:v2 .
-docker run -d --net=host --env-file ./env.ist kubeext-dashboard:v2
+docker run -d --net=host --env-file ./env.list kubeext-dashboard:v2
