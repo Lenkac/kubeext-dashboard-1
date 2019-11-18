da301a5b551f86663b4c627ae9b27a7de4a6e71a
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2019-11-18 15:30:35 +0800

    bad

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker pull node:12.0.0
 docker build -t kubeext-dashboard:v2 .
 mkdir /root/dashboard
-docker run -d --net=host -v /root/dashboard:/home/kubeext-dashboard kubeext-dashboard:v2
+docker run -d --net=host kubeext-dashboard:v2
