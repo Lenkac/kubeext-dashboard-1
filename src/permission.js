9d78b0c2c2a3e2991cb7e6bed193fdc4122faf49
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2019-11-29 11:21:45 +0800

    bad

--- src/permission.js
+++ src/permission.js
@@ -81,15 +81,16 @@ router.beforeEach(async (to, from, next) => {
     else{
       if(!(store.getters.roles && store.getters.roles.length > 0)){
         try {
-          var acutalRoles
           store.dispatch('user/getInfo').then(roles => {
-            acutalRoles = roles;
+            return roles
+          }).then(roles => {
             if (!realRouter) {
               getColumns(process.env.VUE_APP_PROJECTTITLE + '-viewroute').then(response => {
                 realRouter = filterAsyncRouter(response.data)
                 router.addRoutes(realRouter)
-                console.log(acutalRoles.roles)
-                store.dispatch('permission/setRoutes', realRouter,acutalRoles.roles).then(()=>{
+                return realRouter
+              }).then( realRouter => {
+                store.dispatch('permission/setRoutes', realRouter,roles).then(()=>{
                   console.log(realRouter)
                   next({ ...to, replace: true })
                 })
