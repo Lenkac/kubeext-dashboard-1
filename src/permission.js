2303690fa57d5582452581c1bf12519c38145210
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2019-11-29 13:09:35 +0800

    bad

--- src/permission.js
+++ src/permission.js
@@ -128,10 +128,10 @@ router.beforeEach(async (to, from, next) => {
           const { roles } = await store.dispatch('user/getInfo')
 
           // generate accessible routes map based on roles
-          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+          const accessRoutes = await store.dispatch('permission/setRoutes', [roles,realRouter])
 
           // dynamically add accessible routes
-          router.addRoutes(accessRoutes)
+         // router.addRoutes(accessRoutes)
 
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
