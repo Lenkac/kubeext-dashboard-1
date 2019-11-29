96014532b4f8ff22764439f86159d8ffbf42ef16
tteat <495791196@qq.com>
2019-11-29 13:05:48 +0800

    l

--- src/permission.js
+++ src/permission.js
@@ -71,45 +71,93 @@ router.beforeEach(async (to, from, next) => {
   // determine whether the user has logged in
   const hasToken = getToken()
 
+  // if(!realRouter){
+  //   getColumns(process.env.VUE_APP_PROJECTTITLE + '-viewroute').then(response =>{
+  //     realRouter = filterAsyncRouter(response.data)
+  //     router.addRoutes(realRouter)
+  //   })
+  // }
+  // } else {
+  //   if (to.path === '/login') {
+  //     next({ path: '/' })
+  //     NProgress.done()
+  //   }
+  //   else {
+  //     if (!(store.getters.roles && store.getters.roles.length > 0)) {
+  //       // try {
+  //       //   var roles = store.dispatch('user/getInfo')
+  //       //   //var rResponse = getColumns(process.env.VUE_APP_PROJECTTITLE + '-viewroute')
+  //       //   Promise.all([roles]).then(res => {
+  //       //     viewRoles = res[0]
+  //       //     store.dispatch('permission/setRoutes', [viewRoles,realRouter])
+  //       //   })
+  //       //   next({ ...to, replace: true })
+  //       // } catch (error) {
+  //       //   await store.dispatch('user/resetToken')
+  //       //   Message.error(error || 'Has Error')
+  //       //   next(`/login?redirect=${to.path}`)
+  //       //   NProgress.done()
+  //       // }
+  //     }
+  //     else {
+  //       next()
+  //       NProgress.done()
+  //     }
+  //   }
+  // }
   if(!realRouter){
-    getColumns(process.env.VUE_APP_PROJECTTITLE + '-viewroute').then(response =>{
-      realRouter = filterAsyncRouter(res[1].data)
-      router.addRoutes(realRouter)
-    })
+    realRouter = additionalRouter
+    realRouter = filterAsyncRouter(realRouter)
+    router.addRoutes(realRouter)
   }
 
-  if (!hasToken) {
-    next(`/login?redirect=${to.path}`)
-    NProgress.done()
-  } else {
+  if (hasToken) {
     if (to.path === '/login') {
+      // if is logged in, redirect to the home page
       next({ path: '/' })
       NProgress.done()
-    }
-    else {
-      if (!(store.getters.roles && store.getters.roles.length > 0)) {
+    } else {
+      // determine whether the user has obtained his permission roles through getInfo
+      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      if (hasRoles) {
+        next()
+      } else {
         try {
-          var roles = store.dispatch('user/getInfo')
-          var rResponse = getColumns(process.env.VUE_APP_PROJECTTITLE + '-viewroute')
-          Promise.all([roles, rResponse]).then(res => {
-            realRouter = res[1]
-            viewRoles = res[0]
-            store.dispatch('permission/setRoutes', res)
-          })
+          // get user info
+          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+          const { roles } = await store.dispatch('user/getInfo')
+
+          // generate accessible routes map based on roles
+          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+
+          // dynamically add accessible routes
+          router.addRoutes(accessRoutes)
+
+          // hack method to ensure that addRoutes is complete
+          // set the replace: true, so the navigation will not leave a history record
           next({ ...to, replace: true })
         } catch (error) {
+          // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
           Message.error(error || 'Has Error')
           next(`/login?redirect=${to.path}`)
           NProgress.done()
         }
       }
-      else {
-        next()
-        NProgress.done()
-      }
+    }
+  } else {
+    /* has no token*/
+
+    if (whiteList.indexOf(to.path) !== -1) {
+      // in the free login whitelist, go directly
+      next()
+    } else {
+      // other pages that do not have permission to access are redirected to the login page.
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
     }
   }
+
 
 })
 
