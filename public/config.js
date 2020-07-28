0cab7e11de96860419586fb6d2e87383f5a243d6
syswu <wuheng@iscas.ac.cn>
2020-07-28 09:18:15 +0800

    update

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 window.g = {
-    baseURL:"http://39.106.124.113:30308/kubesys/",
-    VUE_APP_MONITOR_HOST:"39.106.124.113",
-    VUE_APP_MONITOR_HOST_GRAFANA:"39.106.124.113:31000",
-    VUE_APP_CONTAINER_HOST:"39.106.124.113",
-    VUE_APP_VNCHOST:"39.106.124.113",
-    registry:"http://39.106.124.113:5000"     
+    baseURL:"http://0.0.0.0:30310/kubesys/",
+    VUE_APP_MONITOR_HOST:"0.0.0.0",
+    VUE_APP_MONITOR_HOST_GRAFANA:"0.0.0.0:31002",
+    VUE_APP_CONTAINER_HOST:"0.0.0.0",
+    VUE_APP_VNCHOST:"0.0.0.0",
+    registry:"http://0.0.0.0:5000"     
 }
